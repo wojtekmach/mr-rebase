658f5ff68cf14d385d8304228114c95ed861d5d7
Wojtek Mach <wojtek@wojtekmach.pl>
2016-01-02 22:14:52 +0100

    Extract with_tmpdir! helper function

--- web/models/rebase.ex
+++ web/models/rebase.ex
@@ -1,16 +1,23 @@
 defmodule Rebase do
   def call(token, org, repo, branch) do
     url = "https://#{token}@github.com/#{org}/#{repo}"
-    dirname = "tmp/repos/#{org}-#{repo}-#{SecureRandom.uuid}"
+
+    with_tmpdir!(fn _dirname ->
+      IO.inspect System.cmd("git", ["clone", url, "."])
+      IO.inspect System.cmd("git", ["fetch", "origin", branch])
+      IO.inspect System.cmd("git", ["checkout", branch])
+      IO.inspect System.cmd("git", ["rebase", "master"])
+      IO.inspect System.cmd("git", ["push", "-f", "origin", branch])
+    end)
+  end
+
+  def with_tmpdir!(f, prefix \\ "") do
+    cwd = System.cwd!
+    dirname = System.tmp_dir! <> "/" <> prefix <> SecureRandom.uuid
     File.mkdir_p!(dirname)
     File.cd!(dirname)
-    System.cmd("git", ["clone", url, "."])
-    IO.inspect System.cmd("git", ["fetch", "origin", branch])
-    IO.inspect System.cmd("git", ["checkout", branch])
-    IO.inspect System.cmd("git", ["rebase", "master"])
-    IO.inspect System.cmd("git", ["push", "-f", "origin", branch])
-
-    File.cd!("../../..")
+    f.(dirname)
+    File.cd!(cwd)
     File.rm_rf!(dirname)
   end
 end

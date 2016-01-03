d3a1d00d189d79b765990ff67ad762fd02e1fb5b
Wojtek Mach <wojtek@wojtekmach.pl>
2016-01-03 22:15:45 +0100

    Minor cleanup

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -6,7 +6,7 @@ defmodule MrRebase.PageController do
     user = get_session(conn, :user)
 
     if access_token do
-      client = Tentacat.Client.new(%{access_token: access_token})
+      client = github_client(access_token)
       repositories = repositories_for_login(client, user.login)
     end
 
@@ -14,10 +14,11 @@ defmodule MrRebase.PageController do
   end
 
   def repo(conn, %{"user" => org, "repo" => repo}) do
-    access_token = get_session(conn, :access_token)
     user = get_session(conn, :user)
-    client = Tentacat.Client.new(%{access_token: access_token})
-    prs = Tentacat.Pulls.filter org, repo, %{state: "open"}, client
+    client = github_client(conn)
+    prs = pull_requests(client, org, repo)
+
+    IO.inspect(client)
 
     render conn, "repo.html", %{org: org, repo: repo, prs: prs}
   end
@@ -31,7 +32,19 @@ defmodule MrRebase.PageController do
     |> redirect(to: "/repos/#{org}/#{repo}")
   end
 
-  def repositories_for_login(client, login) do
+  defp repositories_for_login(client, login) do
     Tentacat.get("users/#{login}/repos", client, %{per_page: 100})
+  end
+
+  defp pull_requests(client, org, repo) do
+    Tentacat.Pulls.filter org, repo, %{state: "open"}, client
+  end
+
+  defp github_client(%Plug.Conn{} = conn) do
+    access_token = get_session(conn, :access_token)
+    github_client(access_token)
+  end
+  defp github_client(access_token) do
+    Tentacat.Client.new(%{access_token: access_token})
   end
 end

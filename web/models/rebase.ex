141e76fdca77a4253d370d5a6a90a55e283dd2ac
Wojtek Mach <wojtek@wojtekmach.pl>
2016-01-06 22:16:42 +0100

    Add sleep

--- web/models/rebase.ex
+++ web/models/rebase.ex
@@ -6,18 +6,22 @@ defmodule Rebase do
       IO.puts "Cloning..."
       IO.inspect System.cmd("git", ["clone", url, "."])
 
+      :timer.sleep(1000)
       IO.inspect "On master branch"
       IO.inspect System.cmd("git", ["log", "--oneline", "-1"])
 
+      :timer.sleep(1000)
       IO.puts "Switching branch..."
       IO.inspect System.cmd("git", ["fetch", "origin", branch])
       IO.inspect System.cmd("git", ["checkout", branch])
       IO.inspect "On #{branch} branch"
       IO.inspect System.cmd("git", ["log", "--oneline", "-1"])
 
+      :timer.sleep(1000)
       IO.puts "Rebasing..."
       IO.inspect System.cmd("git", ["rebase", "master"])
 
+      :timer.sleep(1000)
       IO.puts "Pushing..."
       IO.inspect System.cmd("git", ["push", "-f", "origin", branch])
     end)
